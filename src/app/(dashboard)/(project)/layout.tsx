b5b6788aeb44e7e974ebef8cc2394ae04229b0ad
swaraj bachu <rajswaraj.r@gmail.com>
2023-06-04 21:29:21 +0530

    adding commits page

--- src/app/(dashboard)/(project)/layout.tsx
+++ src/app/(dashboard)/(project)/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
       <section>
         <div className="lg:flex relative gap-8">
         <Sidebar />
-        <div>
+        <div className="mt-8 lg:mt-1 w-full flex justify-center lg:block">
         {children}
         </div>
         </div>
